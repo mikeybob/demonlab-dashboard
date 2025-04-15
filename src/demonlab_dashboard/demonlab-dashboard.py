25662cc626c386c047b776b738764731af726618
Mike Demondad <mike@demonlab.net>
2025-04-15 04:56:40 +0100

    testing rule line in system health panel.

--- src/demonlab_dashboard/demonlab-dashboard.py
+++ src/demonlab_dashboard/demonlab-dashboard.py
@@ -441,9 +441,7 @@ class GridLayoutTest(App):
 
             gsh_widget = self.query_one("#gsh", Static)
             gsh_widget.update(
-                f"[b]General System Health[/b]\n"
-                yield Rule(line_style="double")
-                f"{label}: {next_datapoint}"
+                f"[b]General System Health[/b]\n{label}: {next_datapoint}"
             )
 
             await asyncio.sleep(60)
